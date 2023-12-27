c2545a693fce46e823ce82dd477a1ebc3771e133
Anjala <118836741+anjalaN@users.noreply.github.com>
2023-12-27 20:21:22 +0100

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<,<?<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+//associaty array
+$persons = array("nisal" => "23", "ajanthe" => "59");
+echo "my firend age is " .$persons["nisal"]. "<br>";
+echo "my firend age is " .$persons["ajanthe"]. "<br>";
+    ?>
+</body>
+</html>
